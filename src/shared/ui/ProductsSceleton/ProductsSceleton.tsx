0010588a1cbc5d5f2eaf41defc59c69ab7424532
Poltorak Denys <itwfrontend@gmail.com>
2025-08-20 10:41:32 +0300

    refactor: update skeleton

--- src/shared/ui/ProductsSceleton/ProductsSceleton.tsx
+++ src/shared/ui/ProductsSceleton/ProductsSceleton.tsx
@@ -26,15 +26,21 @@ const ProductsSkeleton = () => (
           </div>
         </div>
 
+        <div className="mt-1 flex gap-2">
+          <div className="relative h-4 w-12 overflow-hidden rounded bg-gray-200">
+            <div className="animate-shimmer absolute inset-0 bg-gradient-to-r from-gray-200 via-gray-300 to-gray-200" />
+          </div>
+          <div className="relative h-4 w-10 overflow-hidden rounded bg-gray-200">
+            <div className="animate-shimmer absolute inset-0 bg-gradient-to-r from-gray-200 via-gray-300 to-gray-200" />
+          </div>
+        </div>
+
         <div className="relative mt-2 h-5 w-20 overflow-hidden rounded bg-gray-200">
           <div className="animate-shimmer absolute inset-0 bg-gradient-to-r from-gray-200 via-gray-300 to-gray-200" />
         </div>
 
         <div className="mt-auto flex gap-2">
-          <div className="relative h-9 flex-1 overflow-hidden rounded-lg bg-gray-200">
-            <div className="animate-shimmer absolute inset-0 bg-gradient-to-r from-gray-200 via-gray-300 to-gray-200" />
-          </div>
-          <div className="relative h-9 flex-1 overflow-hidden rounded-lg bg-gray-200">
+          <div className="relative h-12 flex-1 overflow-hidden rounded-lg bg-gray-200">
             <div className="animate-shimmer absolute inset-0 bg-gradient-to-r from-gray-200 via-gray-300 to-gray-200" />
           </div>
         </div>
